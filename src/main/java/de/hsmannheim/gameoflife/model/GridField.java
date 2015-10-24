774f925fe4391adf6a7858a6e3470baa860cb676
Dennis <dennis.bukowski@gmail.com>
2015-10-24 13:50:44 +0200

    Added Save etc

--- src/main/java/de/hsmannheim/gameoflife/model/GridField.java
+++ src/main/java/de/hsmannheim/gameoflife/model/GridField.java
@@ -1,5 +1,7 @@
 package de.hsmannheim.gameoflife.model;
 
+import java.io.*;
+
 /**
  * Created by Dennis & Alex on 24.10.2015.
  */
@@ -27,5 +29,30 @@ public class GridField {
 
     public int[][] getFieldData() {
         return fieldData;
+    }
+
+    public void save(String path){
+        try {
+            FileOutputStream fOut=new FileOutputStream(path);
+            ObjectOutputStream oOut=new ObjectOutputStream(fOut);
+            oOut.writeObject(fieldData);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void load(String path){
+        try {
+            FileInputStream fIn=new FileInputStream(path);
+            ObjectInputStream oIn=new ObjectInputStream(fIn);
+            Object obj =oIn.readObject();
+            if(obj instanceof int[][]){
+                fieldData= (int[][]) obj;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
