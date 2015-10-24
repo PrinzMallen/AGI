f9a8c3904574f2944c281b6db44f756802252312
Dennis <dennis.bukowski@gmail.com>
2015-10-24 10:48:47 +0200

    Model Class init

---
+++ src/test/java/de/hsmannheim/gameoflife/GridFieldTest.java
@@ -0,0 +1,25 @@
+package de.hsmannheim.gameoflife;
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* GridField Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>Okt 24, 2015</pre> 
+* @version 1.0 
+*/ 
+public class GridFieldTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+
+} 
